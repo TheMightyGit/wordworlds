3026d16c30776f029de6176301039443090e40a9
Johnny Marshall <the.mighty.git@gmail.com>
2022-04-23 18:07:42 +0100

    better letter buttons

--- cartridge/ship.go
+++ cartridge/ship.go
@@ -2,6 +2,7 @@ package cartridge
 
 import (
 	"image"
+	"math/rand"
 	"strings"
 	"time"
 
@@ -63,8 +64,8 @@ func (s *Ship) Start() {
 	s.spriteButtonLetters.Show(GfxBankGfx, s.buttonLettersArea)
 
 	pos := image.Point{0, 0}
-	for rowIdx, letterRow := range []string{"ABCDEFGH", "IJKLMNOP", "QRSTUVWX"} {
-		for letterIdx, letter := range letterRow {
+	for rowIdx, letterRow := range []string{"--------", "--------", "--------"} {
+		for letterIdx := range letterRow {
 			hitBox := image.Rectangle{
 				image.Point{30 * letterIdx, 30 * rowIdx},
 				image.Point{(30 * letterIdx) + 30, (30 * rowIdx) + 30},
@@ -75,7 +76,20 @@ func (s *Ship) Start() {
 			buttonArea := s.buttonLettersArea.GetSubArea(image.Rectangle{pos, pos.Add(image.Point{2, 2})})
 			buttonBgArea := uiArea.GetSubArea(image.Rectangle{pos.Add(image.Point{4, 11}), pos.Add(image.Point{4 + 3, 11 + 3})})
 
-			button := NewLetterButton(buttonArea, buttonBgArea, letter, hitBox)
+			darkBox := darkRedBox
+			brightBox := brightRedBox
+
+			if letterIdx >= 5 {
+				darkBox = darkOrangeBox
+				brightBox = brightOrangeBox
+			} else if letterIdx >= 3 {
+				darkBox = darkGreenBox
+				brightBox = brightGreenBox
+			}
+
+			button := NewLetterButton(buttonArea, buttonBgArea, randomLetter(), hitBox, func(lb *LetterButton) {
+				api.ConsolePrintln(string(lb.letter))
+			}, darkBox, brightBox)
 			s.updateables = append(s.updateables, button)
 			s.clickables = append(s.clickables, button)
 			pos = pos.Add(image.Point{3, 0})
@@ -125,26 +139,48 @@ func (s *Ship) OnClick(pos image.Point) bool {
 	return false
 }
 
-type LetterButton struct {
-	area    marvtypes.MapBankArea
-	bgArea  marvtypes.MapBankArea
-	letter  rune
-	hitBox  image.Rectangle
-	clicked bool
+var (
+	validLetters = []rune("AABCDEEFGHIIJKLMNOOPQRSTUUVWXYYZ")
+)
+
+func randomLetter() rune {
+	return validLetters[rand.Intn(len(validLetters))]
 }
+
+type LetterButton struct {
+	area       marvtypes.MapBankArea
+	bgArea     marvtypes.MapBankArea
+	letter     rune
+	hitBox     image.Rectangle
+	clicked    bool
+	onClick    LetterButtonOnClickFunc
+	disabled   bool
+	enabledBg  *[9]image.Point
+	disabledBg *[9]image.Point
+}
+
+type LetterButtonOnClickFunc func(*LetterButton)
 
 func NewLetterButton(
 	area marvtypes.MapBankArea,
 	bgArea marvtypes.MapBankArea,
 	letter rune,
 	hitBox image.Rectangle,
+	onClick LetterButtonOnClickFunc,
+	enabledBg *[9]image.Point,
+	disabledBg *[9]image.Point,
 ) *LetterButton {
-	return &LetterButton{
-		area:   area,
-		bgArea: bgArea,
-		letter: letter,
-		hitBox: hitBox,
+	b := &LetterButton{
+		area:       area,
+		bgArea:     bgArea,
+		letter:     letter,
+		hitBox:     hitBox,
+		onClick:    onClick,
+		enabledBg:  enabledBg,
+		disabledBg: disabledBg,
 	}
+	b.Enable()
+	return b
 }
 
 func (b *LetterButton) Start() {
@@ -155,20 +191,9 @@ func (b *LetterButton) Update() {
 	if b.clicked {
 		// b.letter = 'X'
 		// drawRune(b.area, image.Point{}, b.letter)
-		b.bgArea.DrawBox(
-			image.Rectangle{image.Point{}, image.Point{2, 2}},
-			&[9]image.Point{
-				{0, 11},
-				{1, 11},
-				{2, 11},
-				{0, 12},
-				{1, 12},
-				{2, 12},
-				{0, 13},
-				{1, 13},
-				{2, 13},
-			},
-			0, 0)
+		b.Disable()
+		b.onClick(b)
+
 		b.clicked = false
 	}
 }
@@ -176,8 +201,59 @@ func (b *LetterButton) Update() {
 func (b *LetterButton) OnClick(pos image.Point) bool {
 	// api.ConsolePrintln(b.letter, b.hitBox, pos)
 	if pos.In(b.hitBox) {
-		b.clicked = true
+		if !b.disabled {
+			b.clicked = true
+		}
 		return true
 	}
 	return false
+}
+
+var (
+	brightRedBox = &[9]image.Point{
+		{0, 11}, {1, 11}, {2, 11},
+		{0, 12}, {1, 12}, {2, 12},
+		{0, 13}, {1, 13}, {2, 13},
+	}
+	darkRedBox = &[9]image.Point{
+		{0, 1}, {1, 1}, {2, 1},
+		{0, 2}, {1, 2}, {2, 2},
+		{0, 3}, {1, 3}, {2, 3},
+	}
+	brightGreenBox = &[9]image.Point{
+		{3, 11}, {4, 11}, {5, 11},
+		{3, 12}, {4, 12}, {5, 12},
+		{3, 13}, {4, 13}, {5, 13},
+	}
+	darkGreenBox = &[9]image.Point{
+		{3, 1}, {4, 1}, {5, 1},
+		{3, 2}, {4, 2}, {5, 2},
+		{3, 3}, {4, 3}, {5, 3},
+	}
+	brightOrangeBox = &[9]image.Point{
+		{6, 11}, {7, 11}, {8, 11},
+		{6, 12}, {7, 12}, {8, 12},
+		{6, 13}, {7, 13}, {8, 13},
+	}
+	darkOrangeBox = &[9]image.Point{
+		{6, 1}, {7, 1}, {8, 1},
+		{6, 2}, {7, 2}, {8, 2},
+		{6, 3}, {7, 3}, {8, 3},
+	}
+)
+
+func (b *LetterButton) Enable() {
+	b.bgArea.DrawBox(
+		image.Rectangle{image.Point{}, image.Point{2, 2}},
+		b.enabledBg,
+		0, 0)
+	b.disabled = false
+}
+
+func (b *LetterButton) Disable() {
+	b.bgArea.DrawBox(
+		image.Rectangle{image.Point{}, image.Point{2, 2}},
+		b.disabledBg,
+		0, 0)
+	b.disabled = true
 }
