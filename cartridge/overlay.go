0f1d686e7300fa978be56fdedbc41572043f4a0e
Johnny Marshall <the.mighty.git@gmail.com>
2022-05-07 20:20:48 +0100

    even more overlay

--- cartridge/overlay.go
+++ cartridge/overlay.go
@@ -16,6 +16,8 @@ type Overlay struct {
 	sprite        marvtypes.Sprite
 	area          marvtypes.MapBankArea
 	usedWordsArea marvtypes.MapBankArea
+	alertArea     marvtypes.MapBankArea
+	statusArea    marvtypes.MapBankArea
 	cursorPos     image.Point
 }
 
@@ -35,6 +37,7 @@ func (o *Overlay) Start() {
 	o.sprite.ChangePos(image.Rectangle{image.Point{0, 0}, image.Point{320, 100}})
 	o.area.ClearWithColour(0, 0, 14, 16)
 	o.sprite.Show(GfxBankSmallFont, o.area)
+
 	o.area.DrawBox(
 		image.Rectangle{
 			image.Point{1, 2},
@@ -44,11 +47,44 @@ func (o *Overlay) Start() {
 		14,
 		16,
 	)
+	o.area.StringToMap(image.Point{3, 2}, 14, 16, "Used Words ")
 	o.usedWordsArea = o.area.GetSubArea(image.Rectangle{
-		image.Point{2, 3},
+		image.Point{1 + 1, 2 + 1},
 		image.Point{16, 12},
 	})
-	o.cursorPos = o.usedWordsArea.StringToMap(image.Point{}, 14, 16, "Used Words:\n")
+
+	o.area.DrawBox(
+		image.Rectangle{
+			image.Point{19, 0},
+			image.Point{60, 3},
+		},
+		outline,
+		14,
+		16,
+	)
+	o.area.StringToMap(image.Point{19 + 1 + 1, 0}, 14, 16, "Alert! ")
+	o.alertArea = o.area.GetSubArea(image.Rectangle{
+		image.Point{19 + 1, 1},
+		image.Point{60, 3},
+	})
+	o.alertArea.StringToMap(image.Point{0, 0}, 14, 16, "Baddie #1: |||||||||||||||||||||||||")
+	o.alertArea.StringToMap(image.Point{0, 1}, 14, 16, "Baddie #2: |||||||||||||||||||||||||")
+
+	o.area.DrawBox(
+		image.Rectangle{
+			image.Point{62 + 1, 2},
+			image.Point{62 + 16, 12},
+		},
+		outline,
+		14,
+		16,
+	)
+	o.area.StringToMap(image.Point{62 + 1 + 2, 2}, 14, 16, "Status ")
+	o.statusArea = o.area.GetSubArea(image.Rectangle{
+		image.Point{62 + 1 + 1, 2 + 1},
+		image.Point{62 + 16, 12},
+	})
+	o.statusArea.StringToMap(image.Point{0, 0}, 14, 16, "Under Attack!\n20 damage\n to shield.")
 }
 
 func (o *Overlay) Update() {
